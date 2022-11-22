f178938566eca8622505ff4f1635d0881b39e563
Jose <joseeenrique.jm@gmail.com>
2022-11-22 13:48:37 -0700

    addes post route

--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
+//get route
 router.get('/', (req, res) => {
+    User.findAll(
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
-    });
+    }));
+});
+//post route
+router.post('/', (req, res) => { 
+    User.create(
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    }));
 });
